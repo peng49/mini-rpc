ae4dd4d8cc95d351e7313c7d96432081a7498900
peng49 <1219955253@qq.com>
2021-01-05 22:03:31 +0800

    fix

---
+++ rpc-client/src/main/java/com/github/peng49/minirpc/client/RpcClient.java
@@ -0,0 +1,23 @@
+package com.github.peng49.minirpc.client;
+
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class RpcClient {
+
+    public <T> T getProxy(Class<T> clazz) {
+        try {
+            return (T) Proxy.newProxyInstance(
+                    getClass().getClassLoader(),
+                    new Class[]{clazz},
+                    (proxy, method, args) -> {
+                        return new Object();
+                    }
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
--- rpc-example/src/main/java/com/github/peng49/minirpc/example/Client.java
+++ rpc-example/src/main/java/com/github/peng49/minirpc/example/Client.java
@@ -1,5 +1,7 @@
 package com.github.peng49.minirpc.example;
 
+import com.github.peng49.minirpc.client.RpcClient;
+
 public class Client {
     public static void main(String[] args) {
         RpcClient client = new RpcClient();
