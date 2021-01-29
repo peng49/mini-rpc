31b4ab86b9ccd7a45e4d23f2fc75bda6fa8d45e1
peng49 <1219955253@qq.com>
2021-01-29 14:28:57 +0800

    fix

--- rpc-example/src/main/java/com/github/peng49/minirpc/example/Server.java
+++ rpc-example/src/main/java/com/github/peng49/minirpc/example/Server.java
@@ -1,8 +1,10 @@
 package com.github.peng49.minirpc.example;
 
+import com.github.peng49.minirpc.server.RpcServer;
+
 public class Server {
     public static void main(String[] args) {
-        RpcServer rpcServer = new RpcServie();
+        RpcServer rpcServer = new RpcServer();
         rpcServer.register(CalcService.class,CalcServiceImpl.class);
         rpcServer.start();
     }
---
+++ rpc-server/src/main/java/com/github/peng49/minirpc/server/RpcServer.java
@@ -0,0 +1,12 @@
+package com.github.peng49.minirpc.server;
+
+public class RpcServer {
+
+    public void register(Class<?> clazz, Class<?> object) {
+
+    }
+
+    public void start() {
+
+    }
+}
