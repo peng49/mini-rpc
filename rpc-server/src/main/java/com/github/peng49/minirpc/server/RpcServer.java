01402197eb16e962cf75e1a771628e84aa38ef0b
peng49 <1219955253@qq.com>
2021-01-31 21:35:04 +0800

    update

--- rpc-server/src/main/java/com/github/peng49/minirpc/server/RpcServer.java
+++ rpc-server/src/main/java/com/github/peng49/minirpc/server/RpcServer.java
@@ -9,4 +9,9 @@ public class RpcServer {
     public void start() {
 
     }
+
+    public void stop()
+    {
+
+    }
 }
---
+++ rpc-server/src/test/java/com/github/peng49/minirpc/server/ServiceInvokerTest.java
@@ -0,0 +1,38 @@
+package com.github.peng49.minirpc.server;
+
+import com.github.peng49.minirpc.common.utils.ReflectionUtils;
+import com.github.peng49.minirpc.proto.Request;
+import com.github.peng49.minirpc.proto.ServiceDescriptor;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+public class ServiceInvokerTest{
+    ServiceManager sm;
+
+    @Before
+    public void before()
+    {
+        this.sm = new ServiceManager();
+
+        TestImpl test = new TestImpl();
+        sm.register(TestInterface.class,test);
+    }
+
+    @Test
+    public void invoker()
+    {
+        Method method = ReflectionUtils.getPublicMethods(TestInterface.class)[0];
+        ServiceDescriptor descriptor = ServiceDescriptor.from(TestInterface.class, method);
+
+        Request request = new Request();
+        request.setService(descriptor);
+
+        ServiceInstance instance = sm.lookup(request);
+
+        ServiceInvoker invoker = new ServiceInvoker();
+
+        Object invoke = invoker.invoke(instance,request);
+    }
+}
